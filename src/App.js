e9175c9944dffa2d5ce16a559fe2212bee679592
Jorge <00jfernandez@gmail.com>
2020-01-12 11:28:37 -0600

    Agregando funcionalidad de rutas src/App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
-import {Link, BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router,Route} from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
+
+  renderHome =()=> <h1>Home</h1>;
+  renderCustomerContainer =()=> <h1>Customer Container</h1>;
+  renderCustomerListContainer =()=> <h1>Customer List Container</h1>;
+  renderCustomerNewContainer =()=> <h1>Customer New Container</h1>;
+
   render(){
     return (
       <Router>
-        <div className="App">
-          <Link to="/Customer">Customers</Link><br></br>
-          <Link to="/Customer/30000000">Customers 30.000.000</Link>
+        <div>
+          <Route exact path="/" component={this.renderHome}></Route>
+          <Route exact path="/customers" component={this.renderCustomerListContainer}></Route>
+          <Route exact path="/customers/:dni" component={this.renderCustomerContainer}></Route>
+          <Route exact path="/customers/new" component={this.renderCustomerNewContainer}></Route>
         </div>
       </Router>
     );
