e043837edbaed85de207682d1c1577fce28a01a5
Jorge <00jfernandez@gmail.com>
2020-01-11 21:18:20 -0600

    Agregar libreria Link y Router, modificar la clase src/App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {Link, BrowserRouter as Router} from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <Router>
+        <div className="App">
+          <Link to="/Customer">Customers</Link><br></br>
+          <Link to="/Customer/30000000">Customers 30.000.000</Link>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
