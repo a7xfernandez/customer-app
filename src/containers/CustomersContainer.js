90257c88ae48b58ee863699c6602475c89519455
Jorge <00jfernandez@gmail.com>
2020-01-16 20:41:03 -0600

    Configurarndo redux-actions en src/actions/fetchCustomers.jssrc/containers/CustomersContainer.js src/store/index.js

--- src/containers/CustomersContainer.js
+++ src/containers/CustomersContainer.js
@@ -63,10 +63,4 @@ CustomersContainer.propTypes = {
 
 };
 
-const mapDispatchToProps =  dispatch => (
-    {
-        fetchCustomers: () => dispatch(fetchCustomers())
-    }
-)
-
-export default withRouter(connect(null, mapDispatchToProps)(CustomersContainer));
+export default withRouter(connect(null, {fetchCustomers})(CustomersContainer));
--- src/store/index.js
+++ src/store/index.js
@@ -4,4 +4,4 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const reducers = state=>state;
 
-export const store = createStore(reducers, {});
+export const store = createStore(reducers, {}, composeEnhancers());
