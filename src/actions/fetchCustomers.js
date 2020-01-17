a3d45766296e65ffb2fe1340f6bc0832cc035d60
Jorge <00jfernandez@gmail.com>
2020-01-16 21:05:24 -0600

    Utilización de DefautlProps, HandleAction y HandleActions

--- src/actions/fetchCustomers.js
+++ src/actions/fetchCustomers.js
@@ -1,4 +1,22 @@
 import {FETCH_CUSTOMERS} from './../constants';
 import {createAction} from 'redux-actions';
 
+const customers= [
+    {
+        "dni": "27000000",
+        "name": "Pedro Paramo",
+        "age": 25
+    },
+    {
+        "dni": "30000000",
+        "name": "Juan Rulfo",
+        "age": 25
+    },
+    {
+        "dni": "37000000",
+        "name": "Pablo Marmol",
+        "age": 25
+    }
+];
+
 export const fetchCustomers = createAction (FETCH_CUSTOMERS);
