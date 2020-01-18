dbba70f9ea00b86f6b44031c502a79cdd28f57e9
Jorge <00jfernandez@gmail.com>
2020-01-17 18:46:34 -0600

    Ruteo dinámico y propiedad children de Route en src/containers/CustomerContainer.js

--- src/containers/CustomerContainer.js
+++ src/containers/CustomerContainer.js
@@ -3,13 +3,20 @@ import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import AppFrame from '../components/AppFrame';
 import {getCustomerByDni} from './../selectors/customers';
+import { Route } from 'react-router-dom';
 
 class CustomerContainer extends Component {
+    //<p>Datos del Cliente "{this.props.customer.name}" </p>
+    renderBody =()=>(
+        <Route path="/customers/:dni/edit" children={
+            ({match})=>(match? <p>Es edición</p>:<p>No es edición</p>)
+        }/>
+    )
     render() {
         return (
             <div>
                 <AppFrame header= {`Cliente ${this.props.dni}`}
-                    body={<p>Datos del Cliente "{this.props.customer.name}" </p>}>
+                    body={this.renderBody()}>
                 </AppFrame>
             </div>
         );
