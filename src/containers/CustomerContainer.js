2a81d2eec9ea4d952ac5c67987b40fff165fcc8a
Jorge <00jfernandez@gmail.com>
2020-01-17 18:34:40 -0600

    Refactor y utilización de reselect

--- src/containers/CustomerContainer.js
+++ src/containers/CustomerContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import AppFrame from '../components/AppFrame';
+import {getCustomerByDni} from './../selectors/customers';
 
 class CustomerContainer extends Component {
     render() {
@@ -21,7 +22,8 @@ CustomerContainer.propTypes = {
 };
 
 const mapStateToProps = (state, props) =>({
-    customer: state.customers.find( c=>c.dni === props.dni)
+    customer: getCustomerByDni(state, props)
 });
 
 export default connect(mapStateToProps,null)(CustomerContainer);
+
