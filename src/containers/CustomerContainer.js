aa175956dbcd77de9909efd0c40a959c23b029e7
Jorge <00jfernandez@gmail.com>
2020-01-23 21:15:52 -0600

    Validacion en el lado del servidor y manejo de errores con submision error

--- src/containers/CustomerContainer.js
+++ src/containers/CustomerContainer.js
@@ -8,6 +8,7 @@ import CustomerEdit from './../components/CustomerEdit';
 import CustomerData from './../components/CustomerData';
 import {fetchCustomers} from './../actions/fetchCustomers';
 import {updateCustomer} from '../actions/updateCustomer';
+import { SubmissionError } from 'redux-form';
 
 class CustomerContainer extends Component {
     
@@ -21,7 +22,11 @@ class CustomerContainer extends Component {
     handleSubmit = values =>{
         console.log(JSON.stringify(values));
         const {id} = values;
-        return this.props.updateCustomer(id,values);
+        return this.props.updateCustomer(id,values).then(r=> {
+            if(r.error){
+                throw new SubmissionError(r.payload);
+            }
+        });
     }
     handleOnBack = () => {
         this.props.history.goBack();
